8e8d71f3337a1950e60a0a5f96ea8a8c2d6c2b4b
Grougie <144916781+Grougie@users.noreply.github.com>
2025-03-04 09:32:08 +0100

    4

--- B0iZo-YE/dungeon-crawler.go
+++ B0iZo-YE/dungeon-crawler.go
@@ -21,13 +21,44 @@ var JumpRight = uint32(8)
 var currentIndex = -1
 var actions = []uint32{0, 0, 0, 1, 2, 1, 0, 0, 4, 1, 1, 1}
 
+
 func NextMove(room string) uint32 {
-	/**
-	/* Insérez votre code ici
-	/*
-	/*                        */
-	currentIndex++
-	fmt.Println("currentIndex", currentIndex)
-	//return actions[currentIndex]
-	return MoveUp
+	roomArray := []rune(room)
+	width := 12
+
+    // Trouver la position de 'P' dans roomArray
+    var pos int
+    found := false
+    for i, r := range roomArray {
+        if r == 'P' {
+            pos = i
+            found = true
+            break
+        }
+    }
+
+    if !found {
+        fmt.Println("P non trouvé dans le tableau")
+        return 4
+    }
+
+    // Vérifier les cases autour
+    fmt.Println("Position de P :", pos)
+
+    // Vérifier Haut
+	if pos-width >= 0 {
+		fmt.Println("Haut :", string(roomArray[pos-width]))
+		return 0
+	} else if pos+width < len(roomArray) {
+		fmt.Println("Bas :", string(roomArray[pos+width]))
+		return 2
+	} else if pos%width != 0 {
+		fmt.Println("Gauche :", string(roomArray[pos-1]))
+		return 1
+	} else if (pos+1)%width != 0 {
+		fmt.Println("Droite :", string(roomArray[pos+1]))
+		return 3
+	}
+
+	return 4
 }
